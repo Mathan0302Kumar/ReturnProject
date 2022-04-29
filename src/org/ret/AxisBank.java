dd691bde0b4afcf24076eab7135a76d0b7f63b1e
Mathan0302Kumar <madhan511kumar@gmail.com>
2022-04-29 15:24:57 +0530

    Return2 Push

--- src/org/ret/AxisBank.java
+++ src/org/ret/AxisBank.java
@@ -4,6 +4,20 @@ public class AxisBank {
 	
 	int a= 10;
 
+private void Fixed(int a,int b,int c) {
+	
+	int d=0;
+	d=a+b+c;
+	
+	System.out.println("My value is :"+d);
 
+}
+
+public static void main(String[] args) {
+	AxisBank nm = new AxisBank();
+	nm.Fixed(10, 20, 30);
+	System.out.println("My instance value is:"+nm.a);
+	
+}
 
 }
